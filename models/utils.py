2be98542a5ccf50b1b4952aede9957d4388c2388
jmf-mas <masakunafelicien@gmail.com>
2023-06-26 13:08:55 -0400

    refinement

--- models/utils.py
+++ models/utils.py
@@ -5,9 +5,11 @@ import torch.nn as nn
 import pickle
 from pathlib import Path
 from sklearn import metrics as sk_metrics
+from sklearn.model_selection import train_test_split
 
 file_extension = ".csv"
 parent_name ="checkpoints/"
+data_directory ="data/"
 
 class Loader(torch.utils.data.Dataset):
     def __init__(self):
@@ -171,6 +173,35 @@ def save(model):
 def load(model):
     with open(parent_name+model.name+".pickle", "rb") as fp:
         model.load_state_dict(pickle.load(fp))
+        
+def save_processed_data(XY, filename, train_rate = .65, val_rate = 0.2):
+    X, y = XY[:, :-1], XY[:, -1]
+    X_n = X[y==0]
+    X_a = X[y==1]
+    n_n = len(X_n)
+    n_a = len(X_a)
+    ranges = [i for i in range(n_n)]
+    selected = np.random.choice(ranges, int(train_rate*n_n), replace = False)
+    X_train = X_n[selected]
+    
+    remained = list(set(ranges).difference(set(selected)))
+    X_n_r = X_n[remained]
+    n_n_r = len(X_n_r)
+    y_test = [0]*n_n_r + [1]*n_a
+    X_test = np.concatenate((X_n_r, X_a), axis=0)
+    
+    X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=val_rate, random_state=42)
+    XY_test = np.concatenate((X_test, y_test.reshape(-1, 1)), axis=1)
+    XY_val = np.concatenate((X_val, y_val.reshape(-1, 1)), axis=1)
+    np.savetxt(data_directory + filename+"_train"+file_extension, X_train, delimiter=",")
+    np.savetxt(data_directory + filename+"_val"+file_extension, XY_val, delimiter=",")
+    np.savetxt(data_directory + filename+"_test"+file_extension, XY_test, delimiter=",")
+    
+    
+                     
+    
+    
+    
         
 
 
