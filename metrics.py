63bf49a5c036b88df1f18683731fac830a43ca9b
jmf-mas <masakunafelicien@gmail.com>
2023-06-26 08:06:57 -0400

    data processing

---
+++ metrics.py
@@ -0,0 +1,57 @@
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
+import numpy as np
+from  scipy.stats import wilcoxon, friedmanchisquare, kendalltau
+
+
+def confusion_matrix_metrics(y_true, y_pred):
+    
+    acc = accuracy_score(y_true, y_pred)
+    prec = precision_score(y_true, y_pred, average='macro')
+    rec = recall_score(y_true, y_pred, average='macro')
+    f1 = f1_score(y_true, y_pred, average='macro')
+    
+    return {"acc":acc, "prec":prec, "rec":rec, "f1":f1}
+
+def inversion_number(E_normal, S_normal, E_abnormal, S_abnormal, eta):
+    
+    E_na = np.array(list(E_normal) + list(E_abnormal))
+    S_na = np.array(list(S_normal) + list(S_abnormal))
+    
+    ES = np.concatenate((E_na.reshape(-1, 1), S_na.reshape(1, -1).T), axis=1)
+    ES_n = np.array(list(filter(lambda e: e[0] <= eta, ES)))
+    ES_a = np.array(list(filter(lambda e: e[0] > eta, ES)))
+    n_n = len(ES_n)
+    n_a = len(ES_a)
+    inn = 0
+    ina = 0
+    for i in range(n_n):
+        for j in range(i + 1, n_n):
+            if (ES_n[i, 1] > ES_n[j, 1]):
+                inn += 1
+    
+    for i in range(n_a):
+        for j in range(i + 1, n_a):
+            if (ES_a[i, 1] < ES_a[j, 1]):
+                ina += 1
+    if n_n >= 2:
+        inn = 2*inn/(n_n*(n_n-1))
+    if n_a >= 2:
+        ina = 2*ina/(n_a*(n_a-1))
+    
+    
+    return inn, ina, (inn + ina)/2
+
+def wilcoxon_test(S1, S2):
+    res = wilcoxon(S1, S2)
+    return res.pvalue
+
+def friedman_test_for_4_samples(S1, S2, S3, S4):
+    res = friedmanchisquare(S1, S2, S3, S4)
+    return res.pvalue
+
+def friedman_test_for_8_samples(S1, S2, S3, S4, S5, S6, S7, S8):
+    res = friedmanchisquare(S1, S2, S3, S4, S5, S6, S7, S8)
+    return res.pvalue
+
+def effect_size(S1, S2):
+    corr, _ = kendalltau(S1, S2)
---
+++ plots.py
@@ -0,0 +1,80 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.pylab import pltlab
+
+def get_heatmap(metrics, filename):
+
+
+    uq_methods = ["CP", "CP+", "EDL", "EDL+",
+                  "MCD", "MCD+", "VAEs", "VAEs+"]
+    
+    metrics = np.round(metrics, 2)
+    
+    
+    fig, ax = plt.subplots()
+    im = ax.imshow(metrics)
+    ax.set_xticks(np.arange(len(uq_methods)))
+    ax.set_yticks(np.arange(len(uq_methods)))
+    ax.set_xticklabels(uq_methods)
+    ax.set_yticklabels(uq_methods)
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+    
+    for i in range(len(uq_methods)):
+        for j in range(len(uq_methods)):
+            text = ax.text(j, i, metrics[i, j], ha="center", va="center", color="w")
+    
+    fig.tight_layout()
+    plt.savefig(filename+".png", dpi=300)
+    plt.show()
+    
+def generate_distribution_plots(normal, abnormal, filename):
+    E_normal, S_normal, S_n, y_normal = normal
+    E_abnormal, S_abnormal, S_a, y_abnormal = abnormal
+    
+    fig, axs = plt.subplots(2, 2)
+    axs[0, 0].plot(E_normal, y_normal, '-b', label='regularity')
+    axs[0, 0].set_ylim(0, 5)
+    axs[0, 0].fill_between(E_normal, y_normal - 2 * S_normal, y_normal + 2 * S_normal, alpha=0.6, color='#86cfac', zorder=5)
+    axs[0, 1].plot(E_normal, y_normal, '-b', label='regularity')
+    axs[0, 1].set_ylim(0, 5)
+    axs[0, 1].fill_between(E_normal, y_normal - 2 * S_n, y_normal + 2 * S_n, alpha=0.6, color='#86cfac', zorder=5)
+    axs[1, 0].plot(E_abnormal, y_abnormal, '-k', label='regularity')
+    axs[1, 0].set_ylim(-1, 3)
+    axs[1, 0].fill_between(E_abnormal, y_abnormal - 2 * S_abnormal, y_abnormal + 2 * S_abnormal, alpha=0.6, color='#ffcccc', zorder=5)
+    axs[1, 1].plot(E_abnormal, y_abnormal, '-k', label='regularity')
+    axs[1, 1].set_ylim(-1, 3)
+    axs[1, 1].fill_between(E_abnormal, y_abnormal - 2 * S_a, y_abnormal + 2 * S_a, alpha=0.6, color='#ffcccc', zorder=5)
+    plt.savefig(filename+".png", dpi=300 )
+    
+def plot_training_loss(cp, edl, mcd, vae, dbname="kdd"):
+
+    filename = "checkpoints/"
+    # Load the training and validation loss dictionaries
+    cp_train_loss = np.loadtxt(filename+"")
+    edl_train_loss = np.loadtxt(filename+"")
+    mcd_train_loss = np.loadtxt(filename+"")
+    vae_train_loss = np.loadtxt(filename+"")
+     
+    # Generate a sequence of integers to represent the epoch numbers
+    epochs = range(1, len(cp_train_loss)+1)
+     
+    # Plot and label the training and validation loss values
+    pltlab.plot(epochs, cp_train_loss, label='Training Loss ')
+    pltlab.plot(epochs, edl_train_loss, label='Training Loss ')
+    pltlab.plot(epochs, mcd_train_loss, label='Training Loss ')
+    pltlab.plot(epochs, vae_train_loss, label='Training Loss ')
+
+     
+    # Add in a title and axes labels
+    pltlab.title('Training Loss')
+    pltlab.xlabel('Epochs')
+    pltlab.ylabel('Loss')
+     
+    # Set the tick locations
+    pltlab.xticks(np.arange(0, len(cp_train_loss)+1, 2))
+     
+    # Display the plot
+    pltlab.legend(loc='best')
+    pltlab.savefig(filename+dbname+"_training.png", dpi=300 )
+    pltlab.show()
